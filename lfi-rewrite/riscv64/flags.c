cc31a9932a5d7cee6877b15492ae9076cf300760
Zachary Yedidia <zyedidia@gmail.com>
2025-07-12 18:23:20 -0700

    Fix riscv64 flags generator

--- lfi-rewrite/riscv64/flags.c
+++ lfi-rewrite/riscv64/flags.c
@@ -8,9 +8,17 @@ riscv64_getflags(enum flags compiler)
 {
     if (args.boxtype == BOX_NONE)
         return "";
+
     char* flags = "";
-    
-    flags = xasprintf("%s -ffixed-x26 -ffixed-x27 -ffixed-x24 -ffixed-x1", flags);
-    
+    switch (compiler) {
+    case FLAGS_POSTLINK:
+        return flags;
+    case FLAGS_CLANG:
+        return flags;
+    case FLAGS_GCC:
+        return xasprintf("%s -ffixed-x26 -ffixed-x27 -ffixed-x24 -ffixed-x1", flags);
+    default:
+        assert(0);
+    }
     return flags;
 }
